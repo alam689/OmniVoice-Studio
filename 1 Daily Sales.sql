77800e0f486f31d98066a4e0c30d23e06f1f0150
Md Khorshed Alam <alam689@gmail.com>
2022-02-28 19:16:54 +0600

    Daily Sales

---
+++ 1 Daily Sales.sql
@@ -0,0 +1,6 @@
+
+select * from monthWiseSales where xyear=2022 and xper=2
+select avg(xqtychl) from DayWiseSales where xyear=2022 and xper=2 and xday<28
+select * from DayWiseSales where xyear=2022 and xper=2 order by xday desc
+
+
