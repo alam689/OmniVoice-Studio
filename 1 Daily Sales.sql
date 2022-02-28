39ecd9064fbe5a902d777c93afdb32880f263eb8
Md Khorshed Alam <alam689@gmail.com>
2022-03-01 01:42:05 +0600

    Update 1 Daily Sales.sql

--- 1 Daily Sales.sql
+++ 1 Daily Sales.sql
@@ -1,4 +1,4 @@
-
+select top 10 * from monthWiseSales order by xqtychl desc
 select * from monthWiseSales where xyear=2022 and xper=2
 select avg(xqtychl) from DayWiseSales where xyear=2022 and xper=2 and xday<28
 select * from DayWiseSales where xyear=2022 and xper=2 order by xday desc
