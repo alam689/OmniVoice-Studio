11f36b2b653b537c99a23c1a18a2450fb41c6a12
Md Khorshed Alam <alam689@gmail.com>
2022-03-10 16:09:44 +0600

    Rate Update Query  modify

--- 1 Daily Sales.sql
+++ 1 Daily Sales.sql
@@ -1,6 +1,6 @@
-select top 10 * from monthWiseSales order by xqtychl desc
+select top 10 * from DayWiseSales order by xqtychl desc
 select * from monthWiseSales where xyear=2022 and xper=3
-select avg(xqtychl) from DayWiseSales where xyear=2022 and xper=2 and xday<28
+select avg(xqtychl)*31 from DayWiseSales where xyear=2022 and xper=3 and xday<8
 select * from DayWiseSales where xyear=2022 and xper=3 order by xday desc
 
 
--- Rate Update.sql
+++ Rate Update.sql
@@ -24,3 +24,33 @@ SELECT         getdate(),  getdate(), zid, 2+ROW_NUMBER() OVER(PARTITION BY xcus
 zactive, zemail, xemail, xwh, xcghdel, xnetrate+5, xdistrict, xlandcost, xdesc FROM            cacuscorrt
 
 
+select xcus,xitem,xnetrate,xnetrate+20,xrem,zutime,getdate()  from cacuscemrate where xcus not like '%NCUS%'
+ and xcus in (select xcus from cacus where  xbloodgrp  in ('NOAKHALI','SYLHET','COMILLA','DHAKA MIDDLE','DHAKA NORTH','DHAKA SOUTH',
+ 'GAZIPUR','MYMENSINGH','NARAYANGONJ')) 
+ order by zutime desc
+
+--pdate cacuscemrate set xnetrate=xnetrate+20,xrem=xnetrate,zutime=getdate()  where xcus not like '%NCUS%'
+ and xcus in (select xcus from cacus where  xbloodgrp  in ('NOAKHALI','SYLHET','COMILLA','DHAKA MIDDLE','DHAKA NORTH','DHAKA SOUTH',
+ 'GAZIPUR','MYMENSINGH','NARAYANGONJ')) 
+
+ select xcus,xitem,xnetrate,xnetrate+25,xrem,zutime,getdate()  from cacuscemrate where xcus not like '%NCUS%'
+ and xcus in (select xcus from cacus where  xbloodgrp  in ('FARIDPUR','KUSHTIA','BARISAL','JESSORE', 'KHULNA',
+ 'NILPHAMARI','RAJSHAHI','RANGPUR','BOGRA')) 
+ order by xcus
+
+ --pdate cacuscemrate set xnetrate=xnetrate+25,xrem=xnetrate,zutime=getdate() where xcus not like '%NCUS%'
+ and xcus in (select xcus from cacus where  xbloodgrp  in ('FARIDPUR','KUSHTIA','BARISAL','JESSORE', 'KHULNA',
+ 'NILPHAMARI','RAJSHAHI','RANGPUR','BOGRA')) 
+ 
+
+
+ --nsert into cacuscemrate
+ select zutime, '', zid, xcus, 'PCMLCRT-2203-00000'+
+ cast(11+ROW_NUMBER() OVER(PARTITION BY xcus ORDER BY xcus) AS varchar(10)), xwh, xitem, xrate, xnetrate, xbonus, xcomm, xrem, '2022-03-10', '', zemail, xemail, zactive, xdesc
+ from cacusrate  where xcus not like '%NCUS%'
+ and xcus in (select xcus from cacus 
+ where xbloodgrp  in ('NOAKHALI','SYLHET','COMILLA','DHAKA MIDDLE','DHAKA NORTH','DHAKA SOUTH',
+ 'GAZIPUR','MYMENSINGH','NARAYANGONJ','FARIDPUR','KUSHTIA','BARISAL','JESSORE', 'KHULNA',
+ 'NILPHAMARI','RAJSHAHI','RANGPUR','BOGRA')) 
+
+
